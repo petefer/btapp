b12e614ff7baee89eaf53b84956b0a47dd040426
Pete Ferreira <29107641+petefer@users.noreply.github.com>
2022-05-31 19:02:28 -0700

    add dialog js

---
+++ wwwroot/fndialog.js
@@ -0,0 +1,369 @@
+window.MyJSMethods =
+{
+    myMethod: function (element) {
+        const msgNoAPI = "Sorry, the dialog API is not supported by this browser.";
+        if (element == 49)
+        {
+            const btn49d1 = document.getElementById('Btn49d1');
+            const dlg49d1 = document.getElementById('Dlg49d1');
+            btn49d1.addEventListener('click', function onOpen() {
+                if (typeof dlg49d1.showModal === "function") {
+                    dlg49d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn49d2 = document.getElementById('Btn49d2');
+            const dlg49d2 = document.getElementById('Dlg49d2');
+            btn49d2.addEventListener('click', function onOpen() {
+                if (typeof dlg49d2.showModal === "function") {
+                    dlg49d2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn49e1 = document.getElementById('Btn49e1');
+            const dlg49e1 = document.getElementById('Dlg49e1');
+            btn49e1.addEventListener('click', function onOpen() {
+                if (typeof dlg49e1.showModal === "function") {
+                    dlg49e1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 50)
+        {
+            const btn50b1 = document.getElementById('Btn50b1');
+            const dlg50b1 = document.getElementById('Dlg50b1');
+            btn50b1.addEventListener('click', function onOpen() {
+                if (typeof dlg50b1.showModal === "function") {
+                    dlg50b1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn50c1 = document.getElementById('Btn50c1');
+            const dlg50c1 = document.getElementById('Dlg50c1');
+            btn50c1.addEventListener('click', function onOpen() {
+                if (typeof dlg50c1.showModal === "function") {
+                    dlg50c1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 51)
+        {
+            const btn51b1 = document.getElementById('Btn51b1');
+            const dlg51b1 = document.getElementById('Dlg51b1');
+            btn51b1.addEventListener('click', function onOpen() {
+                if (typeof dlg51b1.showModal === "function") {
+                    dlg51b1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn51b2 = document.getElementById('Btn51b2');
+            const dlg51b2 = document.getElementById('Dlg51b2');
+            btn51b2.addEventListener('click', function onOpen() {
+                if (typeof dlg51b2.showModal === "function") {
+                    dlg51b2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn51b3 = document.getElementById('Btn51b3');
+            const dlg51b3 = document.getElementById('Dlg51b3');
+            btn51b3.addEventListener('click', function onOpen() {
+                if (typeof dlg51b3.showModal === "function") {
+                    dlg51b3.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn51b4 = document.getElementById('Btn51b4');
+            const dlg51b4 = document.getElementById('Dlg51b4');
+            btn51b4.addEventListener('click', function onOpen() {
+                if (typeof dlg51b4.showModal === "function") {
+                    dlg51b4.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 53)
+        {
+            const btn53c1 = document.getElementById('Btn53c1');
+            const dlg53c1 = document.getElementById('Dlg53c1');
+            btn53c1.addEventListener('click', function onOpen() {
+                if (typeof dlg53c1.showModal === "function") {
+                    dlg53c1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 54)
+        {
+            const btn53c1 = document.getElementById('Btn53c1');
+            const dlg53c1 = document.getElementById('Dlg53c1');
+            btn53c1.addEventListener('click', function onOpen() {
+                if (typeof dlg53c1.showModal === "function") {
+                    dlg53c1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn54d1 = document.getElementById('Btn54d1');
+            const dlg54d1 = document.getElementById('Dlg54d1');
+            btn54d1.addEventListener('click', function onOpen() {
+                if (typeof dlg54d1.showModal === "function") {
+                    dlg54d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 55)
+        {
+            const btn54d1 = document.getElementById('Btn54d1');
+            const dlg54d1 = document.getElementById('Dlg54d1');
+            btn54d1.addEventListener('click', function onOpen() {
+                if (typeof dlg54d1.showModal === "function") {
+                    dlg54d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn55c1 = document.getElementById('Btn55c1');
+            const dlg55c1 = document.getElementById('Dlg55c1');
+            btn55c1.addEventListener('click', function onOpen() {
+                if (typeof dlg55c1.showModal === "function") {
+                    dlg55c1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 56)
+        {
+            const btn56b1 = document.getElementById('Btn56b1');
+            const dlg56b1 = document.getElementById('Dlg56b1');
+            btn56b1.addEventListener('click', function onOpen() {
+                if (typeof dlg56b1.showModal === "function") {
+                    dlg56b1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56b2 = document.getElementById('Btn56b2');
+            const dlg56b2 = document.getElementById('Dlg56b2');
+            btn56b2.addEventListener('click', function onOpen() {
+                if (typeof dlg56b2.showModal === "function") {
+                    dlg56b2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56b3 = document.getElementById('Btn56b3');
+            const dlg56b3 = document.getElementById('Dlg56b3');
+            btn56b3.addEventListener('click', function onOpen() {
+                if (typeof dlg56b3.showModal === "function") {
+                    dlg56b3.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56c1 = document.getElementById('Btn56c1');
+            const dlg56c1 = document.getElementById('Dlg56c1');
+            btn56c1.addEventListener('click', function onOpen() {
+                if (typeof dlg56c1.showModal === "function") {
+                    dlg56c1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56d1 = document.getElementById('Btn56d1');
+            const dlg56d1 = document.getElementById('Dlg56d1');
+            btn56d1.addEventListener('click', function onOpen() {
+                if (typeof dlg56d1.showModal === "function") {
+                    dlg56d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56e1 = document.getElementById('Btn56e1');
+            const dlg56e1 = document.getElementById('Dlg56e1');
+            btn56e1.addEventListener('click', function onOpen() {
+                if (typeof dlg56e1.showModal === "function") {
+                    dlg56e1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56e2 = document.getElementById('Btn56e2');
+            const dlg56e2 = document.getElementById('Dlg56e2');
+            btn56e2.addEventListener('click', function onOpen() {
+                if (typeof dlg56e2.showModal === "function") {
+                    dlg56e2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 57)
+        {
+            const btn56e1 = document.getElementById('Btn56e1');
+            const dlg56e1 = document.getElementById('Dlg56e1');
+            btn56e1.addEventListener('click', function onOpen() {
+                if (typeof dlg56e1.showModal === "function") {
+                    dlg56e1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn56e2 = document.getElementById('Btn56e2');
+            const dlg56e2 = document.getElementById('Dlg56e2');
+            btn56e2.addEventListener('click', function onOpen() {
+                if (typeof dlg56e2.showModal === "function") {
+                    dlg56e2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn57b1 = document.getElementById('Btn57b1');
+            const dlg57b1 = document.getElementById('Dlg57b1');
+            btn57b1.addEventListener('click', function onOpen() {
+                if (typeof dlg57b1.showModal === "function") {
+                    dlg57b1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn57b2 = document.getElementById('Btn57b2');
+            const dlg57b2 = document.getElementById('Dlg57b2');
+            btn57b2.addEventListener('click', function onOpen() {
+                if (typeof dlg57b2.showModal === "function") {
+                    dlg57b2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn57b3 = document.getElementById('Btn57b3');
+            const dlg57b3 = document.getElementById('Dlg57b3');
+            btn57b3.addEventListener('click', function onOpen() {
+                if (typeof dlg57b3.showModal === "function") {
+                    dlg57b3.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn57c1 = document.getElementById('Btn57c1');
+            const dlg57c1 = document.getElementById('Dlg57c1');
+            btn57c1.addEventListener('click', function onOpen() {
+                if (typeof dlg57c1.showModal === "function") {
+                    dlg57c1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 59)
+        {
+            const btn59b1 = document.getElementById('Btn59b1');
+            const dlg59b1 = document.getElementById('Dlg59b1');
+            btn59b1.addEventListener('click', function onOpen() {
+                if (typeof dlg59b1.showModal === "function") {
+                    dlg59b1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 60)
+        {
+            const btn60d1 = document.getElementById('Btn60d1');
+            const dlg60d1 = document.getElementById('Dlg60d1');
+            btn60d1.addEventListener('click', function onOpen() {
+                if (typeof dlg60d1.showModal === "function") {
+                    dlg60d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 61)
+        {
+            const btn60d1 = document.getElementById('Btn60d1');
+            const dlg60d1 = document.getElementById('Dlg60d1');
+            btn60d1.addEventListener('click', function onOpen() {
+                if (typeof dlg60d1.showModal === "function") {
+                    dlg60d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn61e2 = document.getElementById('Btn61e2');
+            const dlg61e2 = document.getElementById('Dlg61e2');
+            btn61e2.addEventListener('click', function onOpen() {
+                if (typeof dlg61e2.showModal === "function") {
+                    dlg61e2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn61e3 = document.getElementById('Btn61e3');
+            const dlg61e3 = document.getElementById('Dlg61e3');
+            btn61e3.addEventListener('click', function onOpen() {
+                if (typeof dlg61e3.showModal === "function") {
+                    dlg61e3.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+        if (element == 62)
+        {
+            const btn62b2 = document.getElementById('Btn62b2');
+            const dlg62b2 = document.getElementById('Dlg62b2');
+            btn62b2.addEventListener('click', function onOpen() {
+                if (typeof dlg62b2.showModal === "function") {
+                    dlg62b2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn62c2 = document.getElementById('Btn62c2');
+            const dlg62c2 = document.getElementById('Dlg62c2');
+            btn62c2.addEventListener('click', function onOpen() {
+                if (typeof dlg62c2.showModal === "function") {
+                    dlg62c2.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            const btn62d1 = document.getElementById('Btn62d1');
+            const dlg62d1 = document.getElementById('Dlg62d1');
+            btn62d1.addEventListener('click', function onOpen() {
+                if (typeof dlg62d1.showModal === "function") {
+                    dlg62d1.showModal();
+                } else {
+                    alert(msgNoAPI);
+                }
+            });
+            return;
+        }
+    }
+};
